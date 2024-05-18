0129138f2f34ec35385c78ae9057cec52c9f6cf7
Nicolas Martinez <nicmart@gmail.com>
2024-05-18 11:28:11 -0700

    add endpoint GET /videos/:id

--- routes/videos.js
+++ routes/videos.js
@@ -2,12 +2,31 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
-const videos = require("../data/videos.json"); // import json data
 
+function loadVideoData() {
+    try {
+        // get file using absolute path using Node path module
+        const path = require('path');
+        const filePath = path.resolve(__dirname, '../data/videos.json');
+        const videos = JSON.parse(fs.readFileSync(filePath, "utf8"));
+        return videos;
+    } catch (error) {
+        console.error('Could not load video data:', error.message);
+    }
+}
 
 // get all videos as a single array collection
 router.get("/", (_req, res) => {
+    const videos = loadVideoData();
     res.json(videos);
+});
+
+
+// get a single video using an id
+router.get("/:id", (req, res) => {
+    const videos = loadVideoData();
+    const foundVideo = videos.find((video) => video.id === req.params.id);
+    res.json(foundVideo);
 });
 
 
